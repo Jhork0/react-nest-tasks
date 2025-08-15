28f578e34c9843991eed704d65e63aa0bdbf7681
Jhork <albeirov30@gmail.com>
2025-08-14 22:32:24 -0500

    Se creo el fron de la app, se configuraron correctamente los estilos de tailswind, y se creo el componente para el envio de informacion al backend

---
+++ frond-tasks/src/components/TaskFrom.tsx
@@ -0,0 +1,52 @@
+import { useState, type ChangeEvent } from "react"
+
+
+
+const TaskFrom = () => {
+    const [task,setTask] =useState({
+    title: "",
+    description: "",
+    done: false
+})
+
+const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement >) =>{
+    setTask({...task, [e.target.name]: e.target.value})
+    
+}
+
+const handleSummit = (e: ChangeEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    console.log(task)
+}
+  return (
+    <div>
+      <form onSubmit={handleSummit}>
+        <input type="text" 
+        name="title" 
+        className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2" 
+        placeholder="Escribe un titulo"
+        onChange={handleChange}
+        />
+        <textarea name="descripcion" 
+        className="border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2" 
+        placeholder="Escribe una descripcion" 
+        rows={3} 
+        onChange={handleChange}
+        id="">
+
+        </textarea>
+
+            <label htmlFor="" className="inline-flex items-center gap-x-2">
+                <input type="checkbox" className="h-5 w-5 text-indigo-400" onChange={()=>{setTask({...task, done: !task.done})}}  />
+                <span>Done</span>
+            </label>
+
+        <button className="bg-indigo-800 px-3 block py-2 w-full text-indigo-200 hover:bg-indigo-500 rounded-lg">
+            Save
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default TaskFrom
